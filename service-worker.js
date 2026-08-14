73915e3c27c431fe8fb939bd356cbf11bec98baf
Stefan Palm <stefan.palm@hotmail.se>
2026-08-14 07:23:47 +0200

    Refresh PWA cache on app update

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,17 @@
-const CACHE = 'dumbbell-days-v6';
+const CACHE = 'dumbbell-days-v7';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg', './icons/icon-192.png', './icons/icon-512.png',
   './assets/calf-raise-form.png', './assets/keystone-dumbbell-deadlift-form.png', './assets/sissy-squat-form.png', './assets/dumbbell-lunge-form.png', './assets/free-squat-form.png',
   './assets/dumbbell-bench-press-form.png', './assets/chest-fly-form.png', './assets/dumbbell-reverse-grip-bench-form.png', './assets/front-raise-form.png', './assets/lateral-raise-form.png', './assets/dumbbell-overhead-press-form.png',
   './assets/decline-dumbbell-triceps-extension-form.png', './assets/bar-dip-form.png', './assets/tate-press-form.png', './assets/dumbbell-bent-row-form.png', './assets/pull-up-form.png', './assets/seated-dumbbell-shrug-form.png', './assets/reverse-fly-form.png', './assets/dumbbell-pullover-form.png', './assets/dumbbell-curl-form.png', './assets/zottman-curl-form.png', './assets/incline-dumbbell-curl-form.png', './assets/crunch-form.png', './assets/russian-twist-form.png', './assets/side-bend-form.png'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => event.waitUntil(
+  caches.open(CACHE)
+    .then(cache => cache.addAll(ASSETS))
+    .then(() => self.skipWaiting())
+));
+
+self.addEventListener('activate', event => event.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+    .then(() => self.clients.claim())
+));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
